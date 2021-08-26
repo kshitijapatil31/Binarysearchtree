92c7c383b284a45e8e3a9eb540c72a4bdba2f561
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-26 14:31:30 +0530

    [kshitja]:searching of element 63 in BST is performed

--- src/com/bridgelabz/bst/MyBST.java
+++ src/com/bridgelabz/bst/MyBST.java
@@ -16,13 +16,16 @@ public class MyBST<T extends Comparable> {
 	public void bstInorder() {
 		inorderRec(root);
 	}
-	public void inorderRec(BSTNode root) {
-		if(root!=null) {
+
+	public void inorderRec(BSTNode<T> root) {
+		if (root != null) {
 			inorderRec(root.left);
-			System.out.println(root.data);
+			System.out.print(root.data + "->");
 			inorderRec(root.right);
 		}
+
 	}
+
 	public BSTNode<T> addBstRecursive(BSTNode<T> root, T data) {
 		if (root == null) {
 			root = new BSTNode<>(data);
@@ -36,6 +39,29 @@ public class MyBST<T extends Comparable> {
 		}
 		return root;
 	}
-	
-	
+
+	public int getSize() {
+		return getSizeRec(root);
+	}
+
+	public int getSizeRec(BSTNode<T> root) {
+		return root == null ? 0 : 1 + getSizeRec(root.left) + getSizeRec(root.right);
+	}
+	boolean search(T data) {
+		root=searchRec(root,data);
+		if(root!=null)
+		return true;
+		else 
+			return false;
+	}
+	public BSTNode <T> searchRec(BSTNode <T>root,T data) {
+		if(root==null || root.data==data) {
+			return root;
+		}
+		int result=data.compareTo(root.data);
+		if(result>0)
+			return searchRec(root.right,data);
+		return searchRec(root.left,data);
+	}
+
 }
